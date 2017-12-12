16c4bb3a028d774098315ab3c8d6749f2f86c62a
Rosalva Venegas Mejia <14470475@itcampeche.edu.mx>
2017-12-11 22:57:25 -0600

    ContadorFragmento: Aplicación con manejo de eventos

--- app/src/main/java/com/rosalvavenegas/contadorfragmentos/ContadorFragment.java
+++ app/src/main/java/com/rosalvavenegas/contadorfragmentos/ContadorFragment.java
@@ -6,12 +6,20 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * Created by Rosalva on 11/12/2017.
  */
 
 public class ContadorFragment extends Fragment {
+    private TextView mEtiquetaConteo;
+    private EditText mCampoConteo;
+    private Button mBotonConteo, mBotonReiniciar;
+    int conteo = 0;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +32,27 @@ public class ContadorFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View v = inflater.inflate(R.layout.fragment_contador,container,false);
+
+        mCampoConteo = (EditText) v.findViewById(R.id.campo_conteo);
+        mBotonConteo = (Button) v.findViewById(R.id.boton_contar);
+        mBotonReiniciar = (Button) v.findViewById(R.id.boton_reiniciar);
+
+        mBotonConteo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conteo++;
+                mCampoConteo.setText("" + conteo);
+            }
+        });
+
+        mBotonReiniciar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                conteo = 0;
+                mCampoConteo.setText(""+ conteo);
+            }
+        });
+
         return v;
     }
 }
