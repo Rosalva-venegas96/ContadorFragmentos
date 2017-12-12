4fe94abecbf6d9dab89cf747ee4f1ed5f1dfdaf5
Rosalva Venegas Mejia <14470475@itcampeche.edu.mx>
2017-12-11 22:38:25 -0600

    ContadorFragmento: el fragmento de interfaz de usuario

--- app/src/main/java/com/rosalvavenegas/contadorfragmentos/MainActivity.java
+++ app/src/main/java/com/rosalvavenegas/contadorfragmentos/MainActivity.java
@@ -1,7 +1,9 @@
 package com.rosalvavenegas.contadorfragmentos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+        if (fragment == null) {
+            fragment = new ContadorFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 }
